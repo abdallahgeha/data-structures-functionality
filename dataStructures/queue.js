6b9a0fb2652a293d38e48d02768c349806e5510c
abdallahgeha <geha.abdallah@gmail.com>
2020-08-26 13:29:53 +0200

    Queue Data Structure

--- dataStructures/queue.js
+++ dataStructures/queue.js
@@ -1,4 +1,4 @@
-class Stack {
+class Queue {
   constructor(maxSize) {
     this.container = [];
     this.maxSize = maxSize || 10;
@@ -16,28 +16,21 @@ class Stack {
     return this.count >= this.maxSize;
   }
 
-  push(data) {
-    if (this.isFull()) return console.log("Stack Overflow!");
+  enqueue(data) {
+    if (this.isFull()) return console.log("Queue Overflow!");
     this.container.push(data);
     return this;
   }
 
-  pop() {
-    if (this.isEmpty()) return console.log("Stack Underflow!");
+  dequeue() {
+    if (this.isEmpty()) return console.log("Queue Underflow!");
 
-    return this.container.pop();
+    return this.container.shift();
   }
 
   peek() {
     if (this.isEmpty()) return null;
-    return this.container[this.container.length - 1];
-  }
-
-  generateFromArray(values) {
-    for (let i = 0; i < values.length; i++) {
-      this.push(values[i]);
-    }
-    return this;
+    return this.container[0];
   }
 
   toArray() {
@@ -45,15 +38,16 @@ class Stack {
   }
 
   clear() {
-    this.head = null;
-    this.length = 0;
+    this.container = [];
   }
 
   print() {
+    let output = "";
     for (let i = this.container.length - 1; i >= 0; i--) {
-      console.log(`[\t${this.container[i]}\t]`);
+      output += `[${this.container[i]}] `;
     }
+    console.log(output);
   }
 }
 
-module.exports = Stack;
+module.exports = Queue;
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const LinkedList = require("./dataStructures/linkedList");
 const Stack = require("./dataStructures/stack");
+const Queue = require('./dataStructures/queue')
 
 function printDataStructures(dataStructures) {
   dataStructures.forEach((dataStructure) => dataStructure.print());
@@ -7,8 +8,10 @@ function printDataStructures(dataStructures) {
 
 let linkedList = new LinkedList();
 let stack = new Stack();
+let queue = new Queue();
 
 linkedList.insertAtHead(1).insertAtHead(2).insertAtHead(3);
 stack.push(1).push(2).push(3);
+queue.enqueue(1).enqueue(2).enqueue(3);
 
-printDataStructures([linkedList, stack]);
+printDataStructures([linkedList, stack, queue]);
